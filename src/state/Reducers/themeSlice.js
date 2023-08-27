1e5b9eb191923be12b90070659cd2974d1a07735
matteo lanci <m.lanci@live.com>
2023-08-27 12:51:57 +0200

    Crated slice for toggleTheme + icon themeSelector in navbar

--- src/state/Reducers/themeSlice.js
+++ src/state/Reducers/themeSlice.js
@@ -1,39 +1,14 @@
-import { createSlice, createAsyncThunk, isRejectedWithValue } from "@reduxjs/toolkit";
-
-import { useState } from "react";
-
-const initialState = {
-  theme: [],
-  loading: false,
-  error: null,
-};
-
-export const handleTheme = createAsyncThunk("theme/handleTheme", async (_, thunkAPI) => {
-  try {
-    const [theme, setTheme] = useState(false);
-  } catch (error) {}
-});
+import { createSlice } from "@reduxjs/toolkit";
 
 const themeSlice = createSlice({
   name: "theme",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(handleTheme.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(handleTheme.fulfilled, (state, action) => {
-        state.loading = false;
-        state.theme = action.payload;
-      })
-      .addCase(handleTheme.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      });
+  initialState: "light",
+  reducers: {
+    toggleTheme: (state) => {
+      return state === "light" ? "dark" : "light";
+    },
   },
 });
 
-export const themeState = (state) => state.theme.theme;
-
+export const { toggleTheme } = themeSlice.actions;
 export default themeSlice.reducer;
